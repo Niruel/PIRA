24b224bd09c077b0d45a78257dd93be98822d6c8
ニルエル <npruppel@gmail.com>
2020-07-05 15:37:52 +0900

    Added Json and set markers

--- pira/src/App.js
+++ pira/src/App.js
@@ -1,23 +1,55 @@
 import React from 'react';
 import './App.css';
+import  cityData from './data/csvjson.json';
+import * as parkData from './data/skateboard-parks.json'
 import {
-GoogleMap,
-useLoadScript,
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+ 
 } from '@react-google-maps/api'
+import { formatRelative } from "date-fns";
 
 
 const libraries = ["places"];
+
 const mapContainerStyle = {
   width: '30vw',
   height: '50vh',
 
 };
 const center = {
-  lat: 35.012856,
-  lng: 135.958981,
+  lat: 51.42459058,
+  lng: -2.400383547,
+
+};
+const options = {
+disableDefaultUI: true,
+zoomControl: true,
 
 };
 export default function App() {
+  const [markers, setMarkers]= React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  
   const {isLoaded,loadError} =  useLoadScript({
     googleMapsApiKey: "AIzaSyAqy7psyJ2_XiXtHPqwrdKcwynMCuQeKgs",
     libraries,
@@ -31,6 +63,25 @@ export default function App() {
   }
   return <div className="MapDiv">
 
-    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={13} center={center}></GoogleMap>
+    <GoogleMap mapContainerStyle={mapContainerStyle}
+              zoom={13}
+              center={center}
+              options={options}
+              onLoad={onMapLoad}>
+                {cityData.map(data =>(
+                   <Marker
+                   key = {data.created}
+                   position= {{
+                    lat:data.latitude, 
+                    lng:data.longitude 
+                  }} 
+                   />
+
+                    
+                ))};
+                
+              </GoogleMap>
+    
+    
   </div>
 }
